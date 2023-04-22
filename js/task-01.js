ca1e5b3656c20791b2a5c77f776d84c8a23b5bc1
OlenaOP <elena.peredriy@gmail.com>
2023-04-22 22:37:03 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ console.log("Number of categories:", listByCategories.children.length);
 
 const itemOfList = document.querySelectorAll(".item");
 
-itemOfList.forEach(function (item, index) {
+itemOfList.forEach((item) => {
   console.log("Category: ", item.firstElementChild.innerHTML);
   console.log("Elements: ", item.lastElementChild.children.length);
 });
