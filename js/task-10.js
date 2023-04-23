2d2e8b5c4de5c7a6060f5162c026c55278654f69
OlenaOP <elena.peredriy@gmail.com>
2023-04-24 00:40:40 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,43 @@
+const input = document.querySelector("input");
+const divBoxes = document.querySelector("#boxes");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+let amount = 0;
+
+input.addEventListener("input", (event) => {
+  amount = event.target.value;
+});
+
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
+
+buttonDestroy.addEventListener("click", () => {
+  destroyBoxes();
+  input.value = "";
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  let size = 30;
+  let markup = "";
+  for (let i = 0; i < amount; i++) {
+    markup += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+    size += 10;
+  }
+  divBoxes.insertAdjacentHTML("beforeend", markup);
+}
+
+buttonCreate.addEventListener("click", () => {
+  if (input.value !== "") {
+    createBoxes(amount);
+  } else {
+    alert("Enter your number");
+  }
+});
